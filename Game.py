eb24de6bbf5b326610ef8e67fad757450c010ef8
paulaanb <paulanb2000@gmail.com>
2021-11-05 11:48:25 +0100

    enunciado creado

--- Game.py
+++ Game.py
@@ -5,3 +5,18 @@ number = random.randint(0,100)
 
 #Generamos el ennunciado del juego
 print("Adivina el número generado desde 0 a 100")
+
+#Nos disponeos a generar un bucle finito al pedir el número
+while True:
+    user_number = int(input("Introduzca el número a adivinar:"))
+
+    #Bases del juego
+    number = int(number)
+    chances = 0
+    if user_number < 0 or user_number > 99:
+    print("El numero elegido no esta dentro del rango solicitado")
+    user_number: int(input("Introduzca el número a adivinar:"))
+    
+    while user_number != number:
+        
+        
