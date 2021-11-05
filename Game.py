17b79f59eeb4aee7614975d88d18b655dc3ac357
paulaanb <paulanb2000@gmail.com>
2021-11-05 11:33:03 +0100

    creamos el numero aleatorio

--- Game.py
+++ Game.py
@@ -1,2 +1,4 @@
 #Creamos el juego donde el jugador debe intoducir un número aleatorio entre 0 y 100, debiendo adivinar dicho número.
-#
+#Metodo para generar el número aleatorio dentro del rango.
+import random
+number = random.randint(0,100)
