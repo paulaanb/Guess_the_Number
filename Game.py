fe9de754f05691a7dc428a8fdb01d8bdb3fff3ab
paulaanb <paulanb2000@gmail.com>
2021-11-05 12:15:42 +0100

    juego final terminado

--- Game.py
+++ Game.py
@@ -3,30 +3,28 @@
 import random
 number = random.randint(0,100)
 
-#Generamos el ennunciado del juego
+#Generamos el enunciado del juego
 print("Adivina el número generado desde 0 a 100")
 
-#Nos disponeos a generar un bucle finito al pedir el número
-while True:
+#Bases del juego
+user_number = int(input("Introduzca el número a adivinar:"))
+chances = 0
+if user_number < 0 or user_number > 99:
+    print("El numero elegido no esta dentro del rango solicitado")
     user_number = int(input("Introduzca el número a adivinar:"))
-
-    #Bases del juego
-    user_number = int(user_number)
-    chances = 0
-    if user_number < 0 or user_number > 99:
-        print("El numero elegido no esta dentro del rango solicitado")
-    user_number: int(input("Introduzca el número a adivinar:"))
     
-    while user_number != number:
-        chances =+ 1
-        if user_number < number:
-            print("\n--> Su número es menor que el generado aleatoriamente.")
-            user_number = int(input("\nIntroduzca el número a adivinar:"))
-        elif user_number > number:
-            print("\n--> Su número es mayor que el generado aleatoriamente.")
-            user_number = int(input("\nIntroduzca el número a adivinar:"))
-    #Salimos del bucle al adivinar el número correcto.
-    if user_number == number:
-        print("\n¡Enhorabuena! Has adivinado el número aleatorio en ", str(chances), "oportunidades") 
-        print("\n¡Bien hecho!")      
+while user_number != number:
+    chances += 1
+    if user_number < number:
+        print("\n--> Su número es menor que el generado aleatoriamente.")
+        user_number = int(input("\nIntroduzca el número a adivinar:"))
+    elif user_number > number:
+        print("\n--> Su número es mayor que el generado aleatoriamente.")
+        user_number = int(input("\nIntroduzca el número a adivinar:"))
+#Salimos del bucle al adivinar el número correcto.
+if user_number == number:
+    chances += 1
+    print("\n¡Enhorabuena! Has adivinado el número aleatorio en" ,str(chances), "oportunidades")
+    print("¡Bien hecho!")  
+        
 #Se terminó el juego
